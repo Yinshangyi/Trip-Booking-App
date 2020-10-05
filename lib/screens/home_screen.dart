710e9e02491516b3a01795b93fc563b2df62ead4
Jean-Loïc De Jaeger <jl.dejaeger@gmail.com>
2020-10-05 03:09:11 +0200

    First commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_ui_starter/hotel_carousel.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_travel_ui_starter/widgets/destination_carousel.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,8 +9,116 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  int _currentTab = 0;
+
+  List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.walking,
+    FontAwesomeIcons.biking
+  ];
+
+  Widget _buildIcon(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+        print(_selectedIndex);
+      },
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          color: _selectedIndex == index
+              ? Theme.of(context).accentColor
+              : Color(0xFFE7EBEE),
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: Icon(
+          _icons[index],
+          size: 25.0,
+          color: _selectedIndex == index
+              ? Theme.of(context).primaryColor
+              : Color(0xFFB4C1C4),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 30.0),
+          children: <Widget>[
+            /* What would you like to find title */
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 120.0),
+              child: Text(
+                'What would you like to find',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            /* Top icons */
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: _icons
+                  .asMap()
+                  .entries
+                  .map(
+                    (MapEntry map) => _buildIcon(map.key),
+                  )
+                  .toList(),
+            ),
+
+            /* Top Destination */
+            SizedBox(height: 20.0),
+            DestinationCarousel(),
+            SizedBox(height: 20.0),
+            HotelCarousel(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentTab,
+        onTap: (int value) {
+          setState(() {
+            _currentTab = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            title: SizedBox.shrink(),
+            icon: Icon(
+              Icons.search,
+              size: 30.0,
+            ),
+          ),
+          BottomNavigationBarItem(
+            title: SizedBox.shrink(),
+            icon: Icon(
+              Icons.local_pizza,
+              size: 30.0,
+            ),
+          ),
+          BottomNavigationBarItem(
+            title: SizedBox.shrink(),
+            icon: CircleAvatar(
+              radius: 15.0,
+              backgroundImage: NetworkImage(
+                  'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/stop-face-sweating-jennifer-lopez-scott-barnes-1575550498.jpg?crop=1xw:0.6675567423230975xh;center,top&resize=640:*'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
